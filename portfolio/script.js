73ee2617cfb7b6347c8dc9ab34c8575634ecdc60
OSJ <tmdwn119@gmail.com>
2025-03-06 07:36:50 +0900

    portfolio upload

---
+++ portfolio/script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".compj01-sw", {
+    navigation: {
+      nextEl: ".compj01-next",
+      prevEl: ".compj01-prev",
+    },
+  });
+  var swiper = new Swiper(".compj02-sw", {
+    navigation: {
+      nextEl: ".compj02-next",
+      prevEl: ".compj02-prev",
+    },
+  });
+  var swiper = new Swiper(".compj03-sw", {
+    navigation: {
+      nextEl: ".compj03-next",
+      prevEl: ".compj03-prev",
+    },
+  });
+  var swiper = new Swiper(".compj04-sw", {
+    navigation: {
+      nextEl: ".compj04-next",
+      prevEl: ".compj04-prev",
+    },
+  });
