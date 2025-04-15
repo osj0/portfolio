21f165aec2c254e95833419a81b613bdc0229c25
OSJ <tmdwn119@gmail.com>
2025-04-16 02:39:51 +0900

    end

--- TRAVEL-VIBES/common/common.js
+++ TRAVEL-VIBES/common/common.js
@@ -8,42 +8,97 @@ $("#header").load("./common/header.html", function() {
         let menuBtn = document.querySelector(".menu");
         let closeBtn = document.querySelector(".closed");
 
-        let searchBtn = document.querySelector(".searchinput");
+        let searchInput = document.querySelector(".sinput");
         let searchCont = document.querySelector(".searchpop");
         let popclBtn = document.querySelector(".popclbtn");
 
         let msearchBtn = document.querySelector(".search");
 
+        const searchBtn = document.querySelector(".searchbtn");
+        let searchList = [];
+
+
+        searchInput.addEventListener("keydown", function(e) {
+            if (e.key === "Enter") {
+                e.preventDefault(); // 폼 전송 막기 (있다면)
+                addSearch();
+            }
+        });
+        searchBtn.addEventListener("click",addSearch);
+            function addSearch() {
+                let taskCont = searchInput.value;
+                const index = searchList.indexOf(taskCont);
+                if (index > -1) searchList.splice(index, 1); 
+                searchList.push(taskCont);
+            
+                if (searchList.length > 7) {
+                    searchList.shift();
+                }
+            
+                render();
+                searchInput.value = "";
+            }
+            
+        
+
+            function render() {
+                let resultHTML = "";
+                
+                // 최근 검색어를 역순으로 출력
+                searchList.slice().reverse().forEach((item, i) => {
+                    const realIndex = searchList.length - 1 - i;
+                    resultHTML += `
+                        <div class="tag">
+                            <p>${item}</p>
+                            <button onclick="deleteSearch(${realIndex})">
+                                <img src="./img/popclbtn.png" alt="삭제">
+                            </button>
+                        </div>
+                    `;
+                });
+            
+                document.querySelector(".hcont").innerHTML = resultHTML;
+            }
+            
+            window.deleteSearch = function(index) {
+                searchList.splice(index, 1);
+                render();
+            };
+            document.querySelector(".history .delete").addEventListener("click", function() {
+                searchList = [];
+                render();
+            });
+            
+            
 
         if (menuBtn) {
             menuBtn.addEventListener("click", () => {
-                console.log("버튼 클릭됨!");
                 document.querySelector(".m-submn-bx").style.display = "block";
             });
         }
         if(closeBtn) {
             closeBtn.addEventListener("click", () => {
-                console.log("버튼 클릭됨!");
                 document.querySelector(".m-submn-bx").style.display = "none";
             });
         }
-        if(searchBtn){
-            searchBtn.addEventListener("click", () => {
-                console.log("search");
+        if(searchInput){
+            searchInput.addEventListener("click", () => {
+                // console.log("input click")
                 searchCont.style.display = "block";
             });
         }
         if(msearchBtn){
             msearchBtn.addEventListener("click", () => {
-                console.log("searchmm");
                 searchCont.style.display = "block";
             });
         }
         if(popclBtn){
             popclBtn.addEventListener("click", () => {
-                console.log("popclBtn");
                 searchCont.style.display = "none";
             });
         }
     }, 100);
 });
+
+//검색창
+
