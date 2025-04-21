1e43411e42b1d52ff7c82f3295f32c88040de699
OSJ <tmdwn119@gmail.com>
2025-04-22 03:24:08 +0900

    about me 수정하기

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,28 +1,11 @@
-var listHTML = $(".pf01-bx").html();
-var listItems = listHTML.split("<div>");
-$(".pf01-bx").html("");
-$.each(listItems, function(i, v) {
-  var item =
-    '<div class="Title-mask"><span class="Title-line">' + v + "</span></div>";
-  $(".pf01-bx").append(item);
-});
-
-// GSAP과 ScrollTrigger 등록
-gsap.registerPlugin(ScrollTrigger);
-
-// pf02-bx가 스크롤에 맞춰 위로 나타나는 애니메이션
-gsap.to('.pf02-bx', {
-  opacity: 1,
-  y: 0, // 위로 올라오기
-  duration: 1,
-  ease: 'power3.out',
-  scrollTrigger: {
-    trigger: '.pf02-bx',  // pf02-bx가 스크롤에 의해 트리거
-    start: 'top 80%',  // 스크롤이 pf02-bx가 화면의 80%에 도달할 때 시작
-    toggleActions: 'play none none none'  // 스크롤 트리거가 활성화되면 애니메이션 시작
-  }
-});
-
-window.onload = function() {
+window.addEventListener('load', () => {
   window.scrollTo(0, 0);
-};
+});
+
+
+setTimeout(() => {
+    document.querySelector('.sloading_bx').classList.add('fade-out');
+    document.querySelector('.main-bx').style.opacity = '1';
+    document.querySelector('.main-bx').style.pointerEvents = 'auto';
+  }, 4800); // 4.8초 뒤 전환 (애니메이션 흐름에 맞춰 조정 가능)
+
